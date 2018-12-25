e0fbf1394ee1ac19fcbce2c5a6e78762354ae24f
Bryce <bryce@brycedev.com>
2018-12-24 20:38:31 -0500

    -

--- lambda/signup.js
+++ lambda/signup.js
@@ -1,4 +1,11 @@
-const request = require("request");
+/**
+ * Minified by jsDelivr using UglifyJS v3.4.4.
+ * Original file: /npm/request@2.88.0/index.js
+ * 
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+"use strict";var extend=require("extend"),cookies=require("./lib/cookies"),helpers=require("./lib/helpers"),paramsHaveRequestBody=helpers.paramsHaveRequestBody;function initParams(e,t,r){"function"==typeof t&&(r=t);var u={};return"object"==typeof t?extend(u,t,{uri:e}):extend(u,"string"==typeof e?{uri:e}:e),u.callback=r||u.callback,u}function request(e,t,r){if(void 0===e)throw new Error("undefined is not a valid uri or options object.");var u=initParams(e,t,r);if("HEAD"===u.method&&paramsHaveRequestBody(u))throw new Error("HTTP HEAD requests MUST NOT include a request body.");return new request.Request(u)}function verbFunc(e){var n=e.toUpperCase();return function(e,t,r){var u=initParams(e,t,r);return u.method=n,request(u,u.callback)}}function wrapRequestMethod(o,s,a,i){return function(e,t,r){var u=initParams(e,t,r),n={};return extend(!0,n,s,u),n.pool=u.pool||s.pool,i&&(n.method=i.toUpperCase()),"function"==typeof a&&(o=a),o(n,n.callback)}}request.get=verbFunc("get"),request.head=verbFunc("head"),request.options=verbFunc("options"),request.post=verbFunc("post"),request.put=verbFunc("put"),request.patch=verbFunc("patch"),request.del=verbFunc("delete"),request.delete=verbFunc("delete"),request.jar=function(e){return cookies.jar(e)},request.cookie=function(e){return cookies.parse(e)},request.defaults=function(t,r){var u=this;"function"==typeof(t=t||{})&&(r=t,t={});var n=wrapRequestMethod(u,t,r);return["get","head","post","put","patch","del","delete"].forEach(function(e){n[e]=wrapRequestMethod(u[e],t,r,e)}),n.cookie=wrapRequestMethod(u.cookie,t,r),n.jar=u.jar,n.defaults=u.defaults,n},request.forever=function(e,t){var r={};return t&&extend(r,t),e&&(r.agentOptions=e),r.forever=!0,request.defaults(r)},(module.exports=request).Request=require("./request"),request.initParams=initParams,Object.defineProperty(request,"debug",{enumerable:!0,get:function(){return request.Request.debug},set:function(e){request.Request.debug=e}});
+//# sourceMappingURL=/sm/f1c30a22bfff6fb917c3f1eb9308cff771a9cc23e3fe8af7c72a6688a1412500.map
 
 const mailChimpAPI = process.env.MAILCHIMP_API_KEY;
 const mailChimpListID = process.env.MAILCHIMP_LIST_ID;
